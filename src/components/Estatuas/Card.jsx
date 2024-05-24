deb399fa92ac5560e2e9ed60d50bfcbfef0fda94
tob-alvarez <tosal1099@gmail.com>
2024-05-24 10:35:44 -0300

    cambios pa mariano

--- src/components/Estatuas/Card.jsx
+++ src/components/Estatuas/Card.jsx
@@ -1,17 +1,11 @@
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+import './Cards.css'
 import image from '../../assets/sf.jpg'
 
 export default function MediaCard() {
     return (
         <Card sx={{ maxWidth: 345 }}>
-            <img src={image} style={{objectFit: 'contain', height: '300px'}} />
-            {/* <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-            </CardActions> */}
+            <img src={image} className='cards' />
         </Card>
     );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -17,6 +17,7 @@ const Home = () => {
               <Card/>
               <Card/>
               <Card/>
+              <Card/>
             </div>
         </div>
     </>
